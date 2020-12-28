7c81a5d6659ec745a854348dc04724669dba3caa
Akinola Peace Olamide <akinolapo@gmail.com>
2020-12-28 21:49:33 +0100

    Update config.py

--- bots/config.py
+++ bots/config.py
@@ -11,10 +11,7 @@ def create_api():
     ACCESS_KEY = environ["ACCESS_KEY"]
     ACCESS_SECRET = environ["ACCESS_SECRET"]
     
-    auth = tweepy.OAuthHandler("olTw7jBNhKwFmMFegs9QX1Amz",
-    "A8tOOyAszr9aKzlI8qFMeENHAqJQJosIMoU6DCn8RacDCR7MUT")
-    auth.set_access_token("1287921782545092610-hvLaUz2oxVIbV4XnjfU2QhmYrIpviP",
-                      "GipJF0YfxMSpFZ6UxH8LGUynorOu3uKWi9aqscrzvI4bw")
+    
     #Verify API
     api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
     # api = tweepy.API(auth)
