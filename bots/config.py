8205c97ecaa407062f9dfba21537005f4c1b83ef
Akinola Peace Olamide <akinolapo@gmail.com>
2021-01-09 13:56:35 +0100

    Update config.py

--- bots/config.py
+++ bots/config.py
@@ -11,6 +11,9 @@ def create_api():
     ACCESS_KEY = environ["ACCESS_KEY"]
     ACCESS_SECRET = environ["ACCESS_SECRET"]
     
+    auth = tweepy.OAuthHandler("CONSUMER_KEY", "CONSUMER_SECRET")
+    auth.set_access_token("ACCESS_KEY","ACCESS_SECRET")
+    
     
     #Verify API
     api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
