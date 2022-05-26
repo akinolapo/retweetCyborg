969369c8385319e3df4102a2a36fb1f24210c708
Peace Akinola <akinolapo@gmail.com>
2022-05-26 10:38:47 +0100

    WIP

--- .history/src/config_20220526103512.js
+++ .history/src/config_20220526103512.js
@@ -6,7 +6,7 @@
  * access_token_secret
  */
 module.exports = {
-    consumer_key: process.env.TWITTER_CONSUMER_KEY,  
+    consumer_key: '',  
     consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
     access_token: process.env.TWITTER_ACCESS_TOKEN,  
     access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
