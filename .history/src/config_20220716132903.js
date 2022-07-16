b050afb24c8ac7015dec73cc29db2d23231b5cf4
Peace Akinola <akinolapo@gmail.com>
2022-07-16 13:33:04 +0100

    WIP

--- .history/src/config_20220716132903.js
+++ .history/src/config_20220716132903.js
@@ -5,7 +5,6 @@
  * access_token
  * access_token_secret
  */
-
  module.exports = {
   consumer_key: process.env.TWITTER_CONSUMER_KEY,  
   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
@@ -13,10 +12,10 @@
   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 }
 
-// module.exports = {
-//   consumer_key: 'JTRvOWMBo7n3EPAK7WYtdSZWO',  
-//   consumer_secret: '5a4W2Ld7tnaRBdczeD3BlwfbHd7Os2IsWn5EWhqw09pnnPEm4K',
-//   access_token: '1287921782545092610-NvqpFOgIQDAcxRS70UbeKwPLZ3HQ9T',  
-//   access_token_secret: 'HGTu40L3gegm5SjvNq6MfV8ePjZA9oRVpnm7mr1Q3ej9p'
-// }
+module.exports = {
+  consumer_key: 'JTRvOWMBo7n3EPAK7WYtdSZWO',  
+  consumer_secret: '5a4W2Ld7tnaRBdczeD3BlwfbHd7Os2IsWn5EWhqw09pnnPEm4K',
+  access_token: '1287921782545092610-NvqpFOgIQDAcxRS70UbeKwPLZ3HQ9T',  
+  access_token_secret: 'HGTu40L3gegm5SjvNq6MfV8ePjZA9oRVpnm7mr1Q3ej9p'
+}
 
